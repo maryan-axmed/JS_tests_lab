f4569fb9c8e174223539fb83d5a2269fa2b3a26d
maryan axmed <12maxmed@gmail.com>
2023-05-03 14:38:01 +0100

    subtraction and multiplication methods testing passed

--- calculator.test.js
+++ calculator.test.js
@@ -37,10 +37,56 @@ describe('sum', () => {
 });
 
 describe('subtract', () => {
+  test('can subtract two small numbers', () => {
+      expected = 10;
+      actual = subtract(30, 20);
+      expect(actual).toBe(expected);
+  });
+
+  test('can substract two large numbers', () => {
+      expected = 150;
+      actual = subtract(400, 250);
+      expect(actual).toBe(expected);
+  });
+
+  test('can subract two negative numbers', () => {
+    expected = -50;
+    actual = subtract(-100, -50)
+    expect(actual).toBe(expected);
+  });
+
+  test('can subract zero', () => {
+    expected = 100;
+    actual = subtract(100, 0)
+    expect(actual).toBe(expected);
+  });
 
 });
 
 describe('multiply', () => {
+  test('can multiply small numbers', () =>{
+    expected = 30;
+    actual = multiply(15, 2);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply large numbers', () =>{
+    expected = 250;
+    actual = multiply(25, 10);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply negative numbers', () =>{
+    expected = 150;
+    actual = multiply(-15, -10);
+    expect(actual).toBe(expected);
+  });
+
+  test('can multiply by zero', () =>{
+    expected = 0;
+    actual = multiply(15, 0);
+    expect(actual).toBe(expected);
+  });
 
 });
 
